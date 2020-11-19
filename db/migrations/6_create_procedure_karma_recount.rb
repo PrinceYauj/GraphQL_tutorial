65008ac47ec520b090d70ee7643e8dc3357aee6d
PrinceYauj <c2h5ohmel@yandex.ru>
2020-11-19 13:35:43 +0300

    Add a procedure to count comment and user karma

---
+++ db/migrations/6_create_procedure_karma_recount.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+
+  up do 
+    run '
+      CREATE PROCEDURE comment_karma_recount() 
+        LANGUAGE PLPGSQL AS $$
+      BEGIN
+        UPDATE comments 
+        SET karma = (
+          SELECT SUM (value) 
+          FROM reactions 
+          WHERE comment_id = comments.id
+        );
+      END;
+      $$;
+
+      CREATE PROCEDURE user_karma_recount() 
+        LANGUAGE PLPGSQL AS $$
+      BEGIN
+        CALL comment_karma_recount();
+        WITH sums AS (
+          SELECT user_id, SUM(karma) 
+          FROM comments 
+          GROUP BY user_id
+        ) 
+        UPDATE users 
+        SET karma = sum 
+        FROM sums 
+        WHERE id = user_id;
+      END;
+      $$;
+    '
+  end
+
+  down do
+    run '
+      DROP PROCEDURE user_karma_recount();
+      DROP PROCEDURE comment_karma_recount();
+    '
+  end
+end
