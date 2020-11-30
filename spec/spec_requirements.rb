58387adc082d215967e0afcdda91447fa8d5970c
PrinceYauj <c2h5ohmel@yandex.ru>
2020-11-30 17:58:52 +0300

    Add spec_requirements

---
+++ spec/spec_requirements.rb
@@ -0,0 +1,11 @@
+require 'sequel'
+require 'graphql'
+require 'factory_bot'
+DB = Sequel.connect('postgres://rails:3@localhost:5432/testdb')
+require_relative '/vagrant/app/models/models'
+require_relative '/vagrant/app/spec/support/database_cleaner'
+require_relative '/vagrant/app/spec/factories/sequel_support'
+require_relative '/vagrant/app/graphql/types/types'
+require_relative '/vagrant/app/graphql/mutations/mutations'
+require_relative '/vagrant/app/graphql/schema'
+require_relative '/vagrant/app/spec/graphql/shared'
